a7c6816f4e88702666162589cfec682c799451fa
Jose Wills <jose.wills3@gmail.com>
2023-08-21 12:12:43 +0100

    connected to database

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ import express from "express";
 import mongoose from "mongoose";
 
 const app = express();
+const mongoString = process.env.DATABASE_URL;
+const database = mongoose.connection;
+const PORT = process.env.PORT;
+
+require("dotenv").config();
 
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log(`Server started at port ${3000}`);
+mongoose.connect(mongoString);
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
 });
