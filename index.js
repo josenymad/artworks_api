d0ab3865611456833c3ff23217f3a863427d09bd
Jose Wills <jose.wills3@gmail.com>
2023-08-21 11:34:48 +0100

    eslint config and prettier

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import express from 'express';
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {console.log(`Server started at port ${3000}`)});
+app.listen(3000, () => {
+  console.log(`Server started at port ${3000}`);
+});
