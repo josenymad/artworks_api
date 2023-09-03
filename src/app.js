a3d94529d21585953a51559357d56537e5a99eb6
Jose Wills <jose.wills3@gmail.com>
2023-09-03 20:46:24 +0100

    enabled cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import routes from "./routes/routes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api", routes);
+app.use(cors);
 
 export default app;
